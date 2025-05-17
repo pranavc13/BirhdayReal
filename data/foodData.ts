7e1a8dcc111d064f5e7709596d68e635a1f10a64
pranavc13 <167692648+pranavc13@users.noreply.github.com>
2025-05-17 17:35:05 +0530

    Update foodData.ts

--- data/foodData.ts
+++ data/foodData.ts
@@ -1,18 +1,18 @@
 export const foodData = {
     lightFood: {
-      dishes: ["Pani puri Vijay Waala", "Sandwich from Sapna Sandwich",],
+      dishes: ["Pani puri Vijay Waala", "Sandwich from Sapna Sandwich","Ramen from Tao Bao"],
       
     },
     heavyFood: {
-      dishes: ["Pizza from GroundBaking"],
+      dishes: ["Pizza from GroundBaking","Chole Bhature from Shreemaya", "Any of your favourite Cafe!"],
       
     },
     dessert: {
-      dishes: [ "Brownies from Theobrama", "Tiramisu agar kahi se mil gya toh"],
+      dishes: [ "Brownies from Theobrama", "Tiramisu agar kahi se mil gya toh","Cheese cake from Pizza Don/Krozzon", "Ice-Cream from Naturals"],
       
     },
     drinks: {
-      dishes: [ "Milkshake from frullato", "Frozen Bottle n Palasia"],
+      dishes: [ "Milkshake from frullato", "Frozen Bottle n Palasia", "Lassi from BJP karyalay", "Coconut Crush from raniseti", "Gola/Soda Vikram Tower"],
       
     },
   }
